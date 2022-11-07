1d7bd7a9abec4b0e856eca69e8b128b0eaeedb21
Vladislav Kulikov <v.kulikov@unreleasedapps.com>
2022-11-07 20:24:07 +0300

    Added BottomSheetScrollPhysics

--- lib/bottom_sheet_scroll_physics.dart
+++ lib/bottom_sheet_scroll_physics.dart
@@ -1,1 +1,3 @@
 library bottom_sheet_scroll_physics;
+
+export 'src/bottom_sheet_scroll_physics.dart' show BottomSheetScrollPhysics;
---
+++ lib/src/bottom_sheet_scroll_physics.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+
+/// Scroll physics that behaves as [ClampingScrollPhysics] at the top of the scroll
+/// and as the default [ScrollPhysics] on the bottom.
+///
+/// This means that on iOS it will behave like [ClampingScrollPhysics] at the top and [BouncingScrollPhysics] at the bottom.
+/// And on Android, it will behave like ClampingScrollPhysics at the top and at the bottom of the scroll.
+///
+/// See also:
+/// - [BouncingScrollPhysics], which is the analogous physics for iOS' bouncing behavior.
+/// - [ClampingScrollPhysics], which is the analogous physics for Android's clamping behavior.
+/// - [ScrollPhysics], for more examples of combining [ScrollPhysics] objects of different types to get the desired scroll physics.
+class BottomSheetScrollPhysics extends ScrollPhysics {
+  /// Creates scroll physics that behaves as [ClampingScrollPhysics] at the top of the scroll
+  /// and as the default [ScrollPhysics] on the bottom.
+  const BottomSheetScrollPhysics({super.parent});
+
+  @override
+  double applyBoundaryConditions(ScrollMetrics position, double value) {
+    // Prevents scrolling over the top of the scroll
+    if (value < position.pixels && position.pixels <= position.minScrollExtent) {
+      // Underscroll
+      return value - position.pixels;
+    } else if (value < position.minScrollExtent && position.minScrollExtent < position.pixels) {
+      // Hit top edge
+      return value - position.minScrollExtent;
+    }
+
+    return super.applyBoundaryConditions(position, value);
+  }
+
+  @override
+  Simulation? createBallisticSimulation(ScrollMetrics position, double velocity) {
+    // It doesn't create a simulation when scrolling over the top of the scroll
+    if (position.pixels <= position.minScrollExtent && velocity < 0.0) {
+      return null;
+    }
+
+    return super.createBallisticSimulation(position, velocity);
+  }
+}
